68258c61f125f4a535d72ee113da9e0db39bedaa
Guilherme Mantoanelli <gbmsom@gmail.com>
2025-08-13 16:34:41 -0300

    Aula

---
+++ PHP.php
@@ -0,0 +1,22 @@
+<?php
+   echo("Hello World <br>");
+//<br = pular linha>
+   $idade = 18;
+//$ sempre antes do nome da variavel, e elas não precisam declarar
+   if ($idade >= 18){
+//       echo("voce é maior de idade.");
+   }  else {
+//       echo("voce é menor de idade");
+   }
+   for($i = 0; $i < 10; $i++) {
+//    echo("<br>O valor de i é: " . $i);
+   }
+
+   while($idade < 65) {
+//    echo("<br>Voce ainda não é idoso. sua idade é: " . $idade);
+    $idade++;
+   }
+
+   $numero (5**3);
+
+   echo (5**3. "<br>");
